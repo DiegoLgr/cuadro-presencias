5259d5f00d65f7904c1486186661c06a912013a1
Diego <diego.lgrip@gmail.com>
2018-04-27 21:12:49 +0200

    Ya se puede rellenar las celdas

--- tabla-presencias.js
+++ tabla-presencias.js
@@ -1,5 +1,5 @@
 // DATOS.
-var workers = ["Juan", "Pepe", "Sebastian", "Carlos"];
+var workers = ["Juan", "Pepe", "Sebastian"];
 var daysShown = 15;
 var data = [];
 for(i=1; i<=daysShown; i++)
@@ -10,8 +10,9 @@ var obra = {
   "codigo": 503,
   "nombre": "Dragados Pozuelo"
 };
-console.log(obra.codigo);
+
 //--------------------------------------------------
+
 var  square = 40,
   padding = 4;
 
@@ -22,13 +23,14 @@ var dayLabelsSize = 15,
     monthLabelsSize = 20,
     workerLabelsSize = 17;
     monthLegendSize = monthLabelsSize + 2*padding,
+
     workerLabelsLength = 150;
 
 var svgWidth = tableWidth + workerLabelsLength,
     svgHeight = tableHeight + monthLegendSize + dayLabelsSize;
 
 
-// SVG.
+// SVG frame.
 var svg = d3.select("#cuadro-presencias").append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight)
@@ -53,54 +55,54 @@ var leftLabels = svg.append("svg")
   .attr("y", monthLegendSize + dayLabelsSize)
   .attr("id", "workers");
 
-var squareRect = svg.append("foreignObject")
+var squareRect = svg.append("svg")
   .attr("width", tableWidth)
   .attr("height", tableHeight)
-  .attr("x", function(){return workerLabelsLength + padding})
-  .attr("y", function(){return monthLegendSize + dayLabelsSize});
+  .attr("x", workerLabelsLength + padding)
+  .attr("y", monthLegendSize + dayLabelsSize)
+  .attr("id", "graph");
 
+//--------------------------------------------------
 // TABLE.
-
-var daysSquare = squareRect.selectAll("div")
+// Se pintan los cuadrados que representan los dias.
+var daysSquare = squareRect.selectAll("squares")
   .data(data).enter()
-  .append("xhtml:div")
-  .html('<div class="rectEnd"></div>')
-  .attr("class", function(d, i){
-     return "day " + d[0] +"-" + d[1];
-   })
+  .append("svg")
   .attr("width", square)
   .attr("height", square)
   .attr("x", function(d){
-    return workerLabelsLength + padding
-      + (d[1] -1) * (square + padding);
+    return  padding + (d[1] -1) * (square + padding);
   })
   .attr("y", function(d){
     for (j=0; j<workers.length; j++)
       if (d[0] === workers[j])
-        return monthLegendSize + dayLabelsSize +
-          padding + j*(square + padding);
+        return padding + j*(square + padding);
   });
+// Se les añade un viv a cada cuadrado para poder
+// cambiar propiedades como el fondo y para poder
+// añadirle un parrafo (con rect no se puede)
+// y en ese parrafo se escriben las cosas.
+squareRect.selectAll("svg").each(function(){
+  d3.select(this).
+  append("foreignObject")
+    .attr("width", square)
+    .attr("height", square)
+    .append("xhtml:div")
+    .attr("class", function(){
+      return "day";
+     })
+    .attr("id", function(d, i){
+      return "" + d[0] +"-" + d[1];
+    })
+    .insert("p")
+    .attr("id", function(d, i){
+      return "" + d[0] +"-" + d[1]+"-p";
+    });
+});
 
-
-
-// var daysSquare = svg.selectAll("squares")
-//   .data(data).enter()
-//   .append("rect")
-//   .attr("class", function(d, i){
-//      return "day " + d[0] +"-" + d[1];
-//    })
-//   .attr("width", square)
-//   .attr("height", square)
-//   .attr("x", function(d){
-//     return workerLabelsLength + padding
-//       + (d[1] -1) * (square + padding);
-//   })
-//   .attr("y", function(d){
-//     for (j=0; j<workers.length; j++)
-//       if (d[0] === workers[j])
-//         return monthLegendSize + dayLabelsSize +
-//           padding + j*(square + padding);
-//   });
+d3.selectAll(".day")
+.style("min-width", function(){return ""+square+"px";})
+.style("min-height", function(){return ""+square+"px";})
 
 // MONT LEGEND.
 var months = ["Junio"];
@@ -150,4 +152,56 @@ var dayLabels = daysLegend.selectAll("text")
     return "" + workerLabelsSize + "px";
   });
 
-var prueba = d3.select("#Pepe-3").append("text").text("funciona");
+d3.select("#Juan-3-p").text(obra.codigo);
+d3.select("#Juan-3").style("background", "#9bf");
+
+d3.select("#Juan-4-p").text(obra.codigo);
+d3.select("#Juan-4").style("background", "#9bf");
+
+d3.select("#Juan-5-p").text(obra.codigo);
+d3.select("#Juan-5").style("background", "#9bf");
+
+d3.select("#Juan-7-p").text(obra.codigo);
+d3.select("#Juan-7").style("background", "#9bf");
+
+d3.select("#Pepe-4-p").text(obra.codigo);
+d3.select("#Pepe-4").style("background", "#9bf");
+
+d3.select("#Pepe-8-p").text(obra.codigo);
+d3.select("#Pepe-8").style("background", "#9bf");
+
+d3.select("#Pepe-9-p").text(obra.codigo);
+d3.select("#Pepe-9").style("background", "#9bf");
+
+d3.select("#Pepe-11-p").text(obra.codigo);
+d3.select("#Pepe-11").style("background", "#9bf");
+
+d3.select("#Pepe-12-p").text(obra.codigo);
+d3.select("#Pepe-12").style("background", "#9bf");
+
+d3.select("#Pepe-13-p").text(obra.codigo);
+d3.select("#Pepe-13").style("background", "#9bf");
+
+d3.select("#Pepe-14-p").text(obra.codigo);
+d3.select("#Pepe-14").style("background", "#9bf");
+
+d3.select("#Pepe-15-p").text(obra.codigo);
+d3.select("#Pepe-15").style("background", "#9bf");
+
+d3.select("#Sebastian-1-p").text(obra.codigo);
+d3.select("#Sebastian-1").style("background", "#9bf");
+
+d3.select("#Sebastian-13-p").text(obra.codigo);
+d3.select("#Sebastian-13").style("background", "#9bf");
+
+d3.select("#Sebastian-14-p").text(obra.codigo);
+d3.select("#Sebastian-14").style("background", "#9bf");
+
+d3.select("#Sebastian-15-p").text(obra.codigo);
+d3.select("#Sebastian-15").style("background", "#9bf");
+
+d3.select("#Sebastian-7-p").text(obra.codigo);
+d3.select("#Sebastian-7").style("background", "#9bf");
+
+d3.select("#Sebastian-8-p").text(obra.codigo);
+d3.select("#Sebastian-8").style("background", "#9bf");
