3508b5e9a06820eb22e529367e18051fbe66ad5b
Diego <diego.lgrip@gmail.com>
2018-04-29 12:53:34 +0200

    ahora es una clase

---
+++ pruebas.js
@@ -0,0 +1,46 @@
+
+let datos = []
+console.log(datos)
+let range = 5
+for (let i=0; i<range; i++){
+  datos.push(i)
+}
+console.log(datos)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function dragon(){
+//   let nombre = "carapolla "
+//   this.apellido = "nosesabe"
+//   that = this
+//   function accion(){
+//     console.log(nombre + "mata")
+//   console.log(that.apellido)
+//   }
+//   this.presentar = function(){
+//     accion()
+//     console.log("hola, soy "+nombre)
+//   }
+// }
+//
+// dragon1 = new dragon()
+//  dragon1.presentar()
--- tabla-presencias.js
+++ tabla-presencias.js
@@ -2,223 +2,216 @@
 // (Esta al final del arcivo).
 
 // DATOS.
-var workers = ["Juan", "Pepe", "Sebastian"];
-var daysShown = 15;
-var daysList = [];
-for(i=1; i<=daysShown; i++)
-  daysList.push(i);
-var data = [];
-for(i=1; i<=daysShown; i++)
-  for(j=0; j<workers.length; j++)
-    data.push([workers[j], i]);
+// var workers = ["Juan", "Pepe", "Sebastian"];
+function CuadroPresencias (){
+  this.workers = ["Juan", "Pepe", "Sebastian"]
+  let daysShown = 15;
+  let daysList = [];
+  for(i=1; i<=daysShown; i++)
+    daysList.push(i);
 
-var obra = {
-  "codigo": 503,
-  "nombre": "Dragados Pozuelo"
-};
+  let data = [];
+  let  square = 40,
+    padding = 4;
+
+  let tableWidth = daysShown * (square + padding) + padding,
+    tableHeight = padding + this.workers.length*(square + padding);
+  // Font sizes.
+  let dayLabelsSize = 15,
+      monthLabelsSize = 20,
+      workerLabelsSize = 17;
+      monthLegendSize = monthLabelsSize + 2*padding,
+
+      workerLabelsLength = 150;
+
+   let svgWidth = tableWidth + workerLabelsLength,
+      svgHeight = tableHeight + monthLegendSize + dayLabelsSize
+ let that = this
 
 //--------------------------------------------------
-
-var  square = 40,
-  padding = 4;
-
-var tableWidth = daysShown * (square + padding) + padding,
-  tableHeight = padding + workers.length*(square + padding);
-// Font sizes.
-var dayLabelsSize = 15,
-    monthLabelsSize = 20,
-    workerLabelsSize = 17;
-    monthLegendSize = monthLabelsSize + 2*padding,
-
-    workerLabelsLength = 150;
-
-var svgWidth = tableWidth + workerLabelsLength,
-    svgHeight = tableHeight + monthLegendSize + dayLabelsSize;
-
-
 // MAQUETADO.
-function crearSvg(){
-  d3.select("#cuadro-presencias")
-  .insert("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight)
-  .attr("id", "svg");
-}
+  function crearSvg(){
+    d3.select("#cuadro-presencias")
+    .insert("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .attr("id", "svg");
+  }
 
-function crearLeyendaMeses(){
-  d3.select("#svg")
-  .insert("svg")
-  .attr("width", tableWidth)
-  .attr("height", monthLegendSize)
-  .attr("x", workerLabelsLength)
-  .attr("id", "monthLegend");
-}
+  function crearLeyendaMeses(){
+    d3.select("#svg")
+    .insert("svg")
+    .attr("width", tableWidth)
+    .attr("height", monthLegendSize)
+    .attr("x", workerLabelsLength)
+    .attr("id", "monthLegend");
+  }
 
-function crearLeyendaDias(){
-  d3.select("#svg")
-  .insert("svg")
-  .attr("width", tableWidth)
-  .attr("height", dayLabelsSize)
-  .attr("x", workerLabelsLength)
-  .attr("y", monthLegendSize)
-  .attr("id", "daysLegend");
-}
+  function crearLeyendaDias(){
+    d3.select("#svg")
+    .insert("svg")
+    .attr("width", tableWidth)
+    .attr("height", dayLabelsSize)
+    .attr("x", workerLabelsLength)
+    .attr("y", monthLegendSize)
+    .attr("id", "daysLegend");
+  }
 
-function crearLeyendaTrabajadores(){
-  d3.select("#svg")
-  .insert("svg")
-  .attr("width", workerLabelsLength)
-  .attr("height", svgHeight)
-  .attr("y", monthLegendSize + dayLabelsSize)
-  .attr("id", "workersLegend");
-}
+  function crearLeyendaTrabajadores(){
+    d3.select("#svg")
+    .insert("svg")
+    .attr("width", workerLabelsLength)
+    .attr("height", svgHeight)
+    .attr("y", monthLegendSize + dayLabelsSize)
+    .attr("id", "workersLegend");
+  }
 
-function crearTabla( ){
-  d3.select("#svg")
-  .insert("svg")
-  .attr("width", tableWidth)
-  .attr("height", tableHeight)
-  .attr("x", workerLabelsLength + padding)
-  .attr("y", monthLegendSize + dayLabelsSize)
-  .attr("id", "graph");
-}
+  function crearTabla( ){
+    d3.select("#svg")
+    .insert("svg")
+    .attr("width", tableWidth)
+    .attr("height", tableHeight)
+    .attr("x", workerLabelsLength + padding)
+    .attr("y", monthLegendSize + dayLabelsSize)
+    .attr("id", "graph");
+  }
 
-//--------------------------------------------------
+  //--------------------------------------------------
 
-// CONTENIDO.
-// Se añaden divs y parrafos como contenido a cada cuadro.
-function crearCuadrados(){
-  // Se pintan los cuadrados que representan los dias.
-  d3.select("#graph")
-  .selectAll("squares")
-  .data(data).enter()
-  .append("svg")
-  .attr("width", square)
-  .attr("height", square)
-  .attr("x", function(d){
-    return  padding + (d[1] -1) * (square + padding);
-  })
-  .attr("y", function(d){
-    for (j=0; j<workers.length; j++)
-      if (d[0] === workers[j])
-        return padding + j*(square + padding);
-  });
-}
-
-function crearFODias(){
-  // Para poder insertar html en el svg.
-  d3.select("#graph")
-  .selectAll("svg")
-  .each(function(){
-    d3.select(this)
-    .append("foreignObject")
+  // CONTENIDO.
+  // Se añaden divs y parrafos como contenido a cada cuadro.
+  function crearCuadrados(){
+    // Se pintan los cuadrados que representan los dias.
+    d3.select("#graph")
+    .selectAll("squares")
+    .data(data).enter()
+    .append("svg")
     .attr("width", square)
-    .attr("height", square);
-  });
-}
+    .attr("height", square)
+    .attr("x", function(d){
+      return  padding + (d[1] -1) * (square + padding);
+    })
+    .attr("y", function(d){
+      for (j=0; j<that.workers.length; j++)
+        if (d[0] === that.workers[j])
+          return padding + j*(square + padding);
+    });
+  }
 
-function crearDivsDias() {
-  // divs como anclaje y para dar formato.
-  d3.select("#graph").selectAll("foreignObject")
-  .append("xhtml:div")
-  .attr("class", function(){
-    return "day";
-   })
-  .attr("id", function(d, i){
-    return "" + d[0] +"-" + d[1];
-  })
-  .style("min-width", function(){return ""+square+"px";})
-  .style("min-height", function(){return ""+square+"px";});
-}
+  function crearFODias(){
+    // Para poder insertar html en el svg.
+    d3.select("#graph")
+    .selectAll("svg")
+    .each(function(){
+      d3.select(this)
+      .append("foreignObject")
+      .attr("width", square)
+      .attr("height", square);
+    });
+  }
 
-function crearContenedorCodigo(){
-  d3.selectAll(".day")
-  .each(function(){
-    d3.select(this)
-    .insert("p")
-    .attr("class", function(){return "codigo-obra";})
-  });
-}
+  function crearDivsDias() {
+    // divs como anclaje y para dar formato.
+    d3.select("#graph").selectAll("foreignObject")
+    .append("xhtml:div")
+    .attr("class", function(){
+      return "day";
+     })
+    .attr("id", function(d, i){
+      return "" + d[0] +"-" + d[1];
+    })
+    .style("min-width", function(){return ""+square+"px";})
+    .style("min-height", function(){return ""+square+"px";});
+  }
 
-function crearContenedorHoras(){
-  d3.selectAll(".day")
+  function crearContenedorCodigo(){
+    d3.selectAll(".day")
     .each(function(){
       d3.select(this)
       .insert("p")
-      .attr("class", function(){return "horas-trabajadas";})
+      .attr("class", function(){return "codigo-obra";})
     });
-}
+  }
 
-function rellenarTabla(){
-  crearCuadrados();
-  crearFODias();
-  crearDivsDias();
-  crearContenedorCodigo();
-  crearContenedorHoras();
-}
+  function crearContenedorHoras(){
+    d3.selectAll(".day")
+      .each(function(){
+        d3.select(this)
+        .insert("p")
+        .attr("class", function(){return "horas-trabajadas";})
+      });
+  }
 
-// LEYENDAS
-var months = ["Junio"];
-function rellenarLeyendaMeses(){
-   d3.select("#monthLegend")
-   .selectAll("text")
-  .data(months).enter()
-  .append("text")
-  .text(function(d){return ""+d+""})
-  .attr("text-anchor", "middle")
-  .attr("x", tableWidth/2)
-  .attr("y", function(){return ""+monthLabelsSize+"px";})
-  .attr("font-size", function(){
-    return "" + monthLabelsSize + "px";
-  });
-}
+  function rellenarTabla(){
+    crearCuadrados();
+    crearFODias();
+    crearDivsDias();
+    crearContenedorCodigo();
+    crearContenedorHoras();
+  }
 
-function rellenarLeyendaDias(){
-  d3.select("#daysLegend")
-  .selectAll("text")
-  .data(daysList).enter()
-  .append("text")
-  .text(function(d){return "" + d + "";})
-  .attr("x", function(d){
-    return 15 + (d - 1) * (square + padding);
-  })
-  .attr("y", function(){
-    return "" + dayLabelsSize + "px";
-  })
-  .attr("font-size", function(){
-    return "" + dayLabelsSize + "px";
-  });
-}
+  // LEYENDAS
+  var months = ["Junio"];
+  function rellenarLeyendaMeses(){
+     d3.select("#monthLegend")
+     .selectAll("text")
+    .data(months).enter()
+    .append("text")
+    .text(function(d){return ""+d+""})
+    .attr("text-anchor", "middle")
+    .attr("x", tableWidth/2)
+    .attr("y", function(){return ""+monthLabelsSize+"px";})
+    .attr("font-size", function(){
+      return "" + monthLabelsSize + "px";
+    });
+  }
 
-function rellenarLeyendaTrabajadores(){
-  d3.select("#workersLegend").selectAll("text")
-  .data(workers).enter()
-  .append("text")
-  .attr("id", "worker")
-  .text(function(d){return d;})
-  .attr("y", function(_,i){
-    return workerLabelsSize +
-      padding + i*(square + padding);
-  })
-  .attr("text-anchor", "end")
-  .attr("x", workerLabelsLength - 10)
-  .attr("font-size", function(){
-    return "" + workerLabelsSize + "px";
-  });
-}
+  function rellenarLeyendaDias(){
+    d3.select("#daysLegend")
+    .selectAll("text")
+    .data(daysList).enter()
+    .append("text")
+    .text(function(d){return "" + d + "";})
+    .attr("x", function(d){
+      return 15 + (d - 1) * (square + padding);
+    })
+    .attr("y", function(){
+      return "" + dayLabelsSize + "px";
+    })
+    .attr("font-size", function(){
+      return "" + dayLabelsSize + "px";
+    });
+  }
 
-//##################################################
-// Funcion final
-function pintarCuadro(){
-  crearSvg();
-  crearLeyendaMeses();
-  crearLeyendaDias();
-  crearLeyendaTrabajadores();
-  crearTabla();
-  rellenarTabla();
-  rellenarLeyendaMeses();
-  rellenarLeyendaDias();
-  rellenarLeyendaTrabajadores();
-}
+  function rellenarLeyendaTrabajadores(){
+    d3.select("#workersLegend").selectAll("text")
+    .data(that.workers).enter()
+    .append("text")
+    .attr("id", "worker")
+    .text(function(d){return d;})
+    .attr("y", function(_,i){
+      return workerLabelsSize +
+        padding + i*(square + padding);
+    })
+    .attr("text-anchor", "end")
+    .attr("x", workerLabelsLength - 10)
+    .attr("font-size", function(){
+      return "" + workerLabelsSize + "px";
+    });
+  }
 
-//##################################################
+  //##################################################
+  // Funcion final
+  this.pintarCuadro = function(){
+    crearSvg();
+    crearLeyendaMeses();
+    crearLeyendaDias();
+    crearLeyendaTrabajadores();
+    crearTabla();
+    rellenarTabla();
+    rellenarLeyendaMeses();
+    rellenarLeyendaDias();
+    rellenarLeyendaTrabajadores();
+  }
+}
+  //##################################################
