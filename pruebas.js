ad6a1192c28472334d631bc6113170504677a983
Diego <diego.lgrip@gmail.com>
2018-04-30 16:30:52 +0200

    Añadida la funcionalidad basica. Siguiente: Pasar a Angular y ordenar el codigo.

--- pruebas.js
+++ pruebas.js
@@ -1,46 +1,43 @@
+const anadirForm = document.forms['anadir-trabajador']
+anadirForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const value = anadirForm
+    .querySelector('input[type="text"]')
+    .value
+  trabajadores.anadirTrabajador(value)
+  cuadro.actualizarCuadro()
+})
 
-let datos = []
-console.log(datos)
-let range = 5
-for (let i=0; i<range; i++){
-  datos.push(i)
-}
-console.log(datos)
+const eliminarForm = document.forms['eliminar-trabajador']
+eliminarForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const value = eliminarForm
+    .querySelector('input[type="text"]')
+    .value
+  trabajadores.eliminarTrabajador(value)
+  cuadro.actualizarCuadro()
+})
 
+const asignarForm = document.forms['asignar-trabajo']
+asignarForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const nombre = asignarForm
+    .querySelector("#nombre")
+    .value
 
+  const dias = asignarForm
+    .querySelector("#dia")
+    .value
 
+  const horas = asignarForm
+    .querySelector("#horas")
+    .value
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function dragon(){
-//   let nombre = "carapolla "
-//   this.apellido = "nosesabe"
-//   that = this
-//   function accion(){
-//     console.log(nombre + "mata")
-//   console.log(that.apellido)
-//   }
-//   this.presentar = function(){
-//     accion()
-//     console.log("hola, soy "+nombre)
-//   }
-// }
-//
-// dragon1 = new dragon()
-//  dragon1.presentar()
+  const obra = asignarForm
+    .querySelector("#obra")
+    .value
+  obras
+  trabajadores.asignarTrabajo(
+    nombre, dias, horas, obras[obra])
+  cuadro.actualizarCuadro()
+})
